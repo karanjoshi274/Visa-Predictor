07835bde4191941fcb8a7773141a9e8ae0f780b4
karanjoshi274 <karanjoshi901@gmail.com>
2025-12-03 16:21:38 +0530

    Changing SMTP Port

--- app/email_utils.py
+++ app/email_utils.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 465))
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 FROM_ADDR = os.getenv("FROM_ADDR", SMTP_USER)
